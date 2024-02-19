c523d7b20de75f93313e10b6d4a27d106251081a
AhmedElmasry1985 <30432819+AhmedElmasry1985@users.noreply.github.com>
2024-02-19 11:36:12 +0200

    initial commit

---
+++ ReceiverApp/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const string filePath = @".\mounted\message.txt";
+Console.WriteLine("Receiver Application Started");
+Console.WriteLine($"I will read the content of the file in {filePath} every 5 sec.");
+while (true)
+{
+    Thread.Sleep(5000);
+    Console.WriteLine(File.Exists(filePath)
+        ? File.ReadAllText(filePath)
+        : "File not exist, create it and wait for 5 sec.");
+}
---
+++ SenderApp/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const string filePath = @".\mounted\message.txt";
+Console.WriteLine("Sender Application Started");
+Console.WriteLine($"I will Write what you type in the file in {filePath}");
+while (true)
+{
+    Console.WriteLine("Write a message:");
+    var txt = Console.ReadLine();
+    if(File.Exists(filePath)) 
+        File.WriteAllText(filePath,txt);  
+    else Console.WriteLine("File not exist, create it and wait for 5 sec.");
+}
+
+// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, What is your name?");
+// var name = Console.ReadLine();
+// Console.WriteLine($"Welcome {name}");
